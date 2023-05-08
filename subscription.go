2acb1fcbc58a58e8b52e04c2d3c84c367c539077
Lync <golangaddict@gmail.com>
2023-05-08 15:57:40 -0400

    upd

---
+++ subscription.go
@@ -0,0 +1,44 @@
+package paddle
+
+type GetSubscriptionResult struct {
+	Success  bool `json:"success"`
+	Response struct {
+		URL string `json:"url"`
+	} `json:"response"`
+}
+
+type GetSubscriptionParams struct {
+	SubscriptionID string `json:"subscription_id"`
+}
+
+type CancelSubscriptionResult struct {
+	Success bool `json:"success"`
+}
+
+type CancelSubscriptionParams struct {
+	SubscriptionID string `json:"subscription_id"`
+}
+
+type SubscriptionService struct {
+	client *Client
+}
+
+func NewSubscriptionService(client *Client) *SubscriptionService {
+	return &SubscriptionService{
+		client: client,
+	}
+}
+
+func (s *SubscriptionService) Get(subID string) (*GetSubscriptionResult, error) {
+	var result GetSubscriptionResult
+	return &result, s.client.Do("POST", "/subscription/users", &GetSubscriptionParams{SubscriptionID: subID}, &result)
+}
+
+func (s *SubscriptionService) Cancel(subID string) (*CancelSubscriptionResult, error) {
+	var result CancelSubscriptionResult
+	return &result, s.client.Do("POST", "/subscription/users_cancel", &CancelSubscriptionParams{SubscriptionID: subID}, &result)
+}
+
+//func (s *SubscriptionService) Cancel(subID string) error {
+//
+//}
